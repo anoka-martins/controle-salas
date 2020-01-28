5c1f03fae6fe9186baac77ebdd10916b7a65f916
Ana Martins <karolineana821@gmail.com>
2020-01-28 16:27:36 -0300

    Atualizando com Verificador de login e permissões

--- reuniozinha/app/src/main/java/com/example/reuniozinha/ui/activities/LoginActivity.java
+++ reuniozinha/app/src/main/java/com/example/reuniozinha/ui/activities/LoginActivity.java
@@ -1,12 +1,13 @@
 package com.example.reuniozinha.ui.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.reuniozinha.R;
 
@@ -24,54 +25,66 @@ public class LoginActivity extends AppCompatActivity {
         final EditText emailUsuario = findViewById(R.id.id_campo_email);
         final EditText senhaUsuario = findViewById(R.id.id_campo_senha);
 
-        String emailString = emailUsuario.getText().toString();
-        String senhaString = senhaUsuario.getText().toString();
-
         Button botaoEntrar = findViewById(R.id.id_botao_login);
         botaoEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(
-                        LoginActivity.this,
-                        ListaEventosSalasActivity.class));
+                try {
+                    String emailUsuarioStr = emailUsuario.getText().toString();
+                    String senhaUsuarioStr = senhaUsuario.getText().toString();
+                    String respostaMetodo = new Verificador().execute(emailUsuarioStr, senhaUsuarioStr).get();
+
+                    if (respostaMetodo.equals("Login efetuado com sucesso!")) {
+                                startActivity(new Intent(
+                                        LoginActivity.this,
+                                        ListaEventosSalasActivity.class));
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Credenciais inválidas!", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                } catch (Exception e) {
+                    Toast.makeText(LoginActivity.this, "Erro ao efetuar login!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
         Button botaoCadastrar = findViewById(R.id.id_botao_cadastrar);
-        botaoCadastrar.setOnClickListener(new View.OnClickListener(){
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
                 startActivity(new Intent(
                         LoginActivity.this,
-                       CadastroColaboradorActivity.class));
+                        CadastroColaboradorActivity.class));
             }
         });
     }
 
-    public static String makeAuthRequest(String email, String password) throws Exception {
-        String wsURL = "http://localhost:8080/ReservaDeSala/rest/usuario/login";
-
-        try {
-            StringBuilder result = new StringBuilder();
-            URL url = new URL(wsURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("authorization", "secret");
-            conn.setRequestProperty("email", email);
-            conn.setRequestProperty("password", password);
-
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-            rd.close();
-            return result.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Erro";
-        }
-    }
+//    public static String makeAuthRequest(String email, String password) throws Exception {
+//        String wsURL = "http://localhost:8080/ReservaDeSala/rest/usuario/login";
+//
+//        try {
+//            StringBuilder result = new StringBuilder();
+//            URL url = new URL(wsURL);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            conn.setRequestMethod("GET");
+//            conn.setRequestProperty("authorization", "secret");
+//            conn.setRequestProperty("email", email);
+//            conn.setRequestProperty("password", password);
+//
+//            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//            String line;
+//            while ((line = rd.readLine()) != null) {
+//                result.append(line);
+//            }
+//            rd.close();
+//            return result.toString();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return "Erro";
+//        }
+//    }
 }
